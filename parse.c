cbbec8f52fd383af9c49aca387cf19f8396ea531
Jonas S Karlsson <jsk@yesco.org>
2023-11-15 06:16:12 +0700

    if and loop/break/cont seems to work

--- parse.c
+++ parse.c
@@ -6,10 +6,6 @@
 // char==name of NonTerminals
 char* NT[128]= {0};
 
-int nont(char t) {
-  return t>='A' && t<='Z';
-}
-
 int eor(char* r) {
   return !*r || *r=='\n' || *r=='|';
 }
@@ -22,10 +18,13 @@ char* match(char r, char* s) {
   // one space matches zero or more
   //if (r==' ') { while(isspace(*s)) s++; return s; }
   // spaceis always removed
-  printf("\nMATCH '%c' '%s'\n", r, s);
+  printf("\nMATCH '%c' %d '%s'\n", r, r, s);
   while(isspace(*s)) s++;
+  if (r=='a'+128) return isalpha(*s) || *s=='_' ? s+1 : NULL;
+  if (r=='d'+128) return isdigit(*s) ? s+1 : NULL;
+  if (r=='w'+128) return isalnum(*s) || *s=='_' ? s+1 : NULL;
   if (r==*s) return s+1;
-  if (nont(r)) return parset(r, s);
+  if (isupper(r)) return parset(r, s);
   return NULL;
 }
   
@@ -88,23 +87,30 @@ char* parset_hlp(char target, char* s) {
     if (*r=='[') return parset_gen(r, s, p);
     //if (*r=='{') return eval(r, s, p); // TODO:
     // $, $; - delimited-list
-    int min=1, max=1;
+    int min=1, max=1, delim= 0;
+    char c= *r;
     if (*r=='$') {
-      switch(*++r) {
-      case ',': case ';': break; // TODO
-      case '/': break; // TODO: rest is optional
+      c= *++r;
+      delim= strchr(",; ", c)?c:0;
+      min= (c=='?'||c=='*'||delim)?0:1;
+      max= (c=='+'||c=='*'||delim)?9999:1;
+      if (strchr("?*+,; ", c)) c= *++r;
+      switch(c) {
+      case 'a': case 'd': case 'w': c=c+128; printf("\nCCCC %d\n", c); break;
+      case '/': c=*++r; break; // TODO: rest is optional
       }
-      min= (*r=='?'||*r=='*')?0:1;
-      max= (*r=='+'||*r=='*')?9999:1;
-      r++;
     }
-    char* m, *last= m;
+    char* m, *last= NULL;
     // TODO: concat captures?
-    while(max-- && (m=match(*r, p))) last=p, p= m;
-    if (min && !m) {
+    while(max-- && (m=match(c, p))) {
+      if (*m && *m==delim) m++;
+      last= p= m;
+    }
+    if (min && !last) {
       // failed: look for alt
       while(*r && *r++!='|');
       ok= 0, p= s;
+      printf("\nFAILED\n");
     } else ok= 1, r++; //, p= m, 
     printf("  '%s'\t%s", p, r);
   }
@@ -138,11 +144,6 @@ int main(void) {
   // longer match first 33
   //NT['D']= "0|1|2|33|3|4|5|6|7|8|9\n";
   NT['D']= "0|1|2|3|4|5|6|7|8|9\n";
-  NT['N']= "DN|D\n";
-  NT['Q']= "select N, N from int(N, N) i\n";
-  NT['Q']= "select N, N from int(N, N) i [ QUERY($$)\n COLS($1,$2)\n IOTA($3,$4) ]\n";
-  NT['O']= "a$+ba";
-  
   test('D', "0");
   test('D', "1");
   test('D', "7");
@@ -151,16 +152,30 @@ int main(void) {
   test('D', "44");
   test('D', "a");
 
+  NT['N']= "DN|D\n";
   test('N', "42");
   test('N', "428");
   test('N', "4");
   test('N', "a428");
 
+  NT['Q']= "select N, N from int(N, N) i\n";
+  NT['Q']= "select N, N from int(N, N) i [ QUERY($$)\n COLS($1,$2)\n IOTA($3,$4) ]\n";
   test('Q', "select 2222,123456 from int(1, 10) i");
 
+  NT['O']= "a$+ba";
+  // not work to match single letter!
   test('O', "aa");
   test('O', "aba");
   test('O', "abba");
+
+  NT['I']= "$+a";
+  test('I', "foo");
+
+  NT['L']= "$,N";
+  test('L', "1,22,333,4444");
+
+  NT['W']= "$+a\n"; // TODO: err matches space lol... as it's skipped
+  NT['S']= "$*W";
+  
+  test('W', "foo bar fie fum");
 }
-
-
---
+++ readme.md
@@ -0,0 +1,9 @@
+# names
+
+small parser+interpreter:
+
+grokie - to grok a little -ie
+
+parsie - small parser?
+parslet - a tiny parser
+pars - forth, lol
