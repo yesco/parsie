6ef609d0177cb6a95b546d2d5a550d0ce51ed983
Jonas S Karlsson <jsk@yesco.org>
2023-11-16 19:30:07 +0700

    readparser commands

--- parse.c
+++ parse.c
@@ -3,6 +3,8 @@
 #include <ctype.h>
 #include <assert.h>
 
+int debug= 1;
+
 // char==name of NonTerminals
 char* NT[128]= {0};
 
@@ -18,7 +20,7 @@ char* match(char r, char* s) {
   // one space matches zero or more
   //if (r==' ') { while(isspace(*s)) s++; return s; }
   // spaceis always removed
-  printf("\nMATCH '%c' %d '%s'\n", r, r, s);
+  if (debug>=3) printf("\n\tMATCH '%c' %d '%s'\n", r, r, s);
   while(isspace(*s)) s++;
   if (r=='a'+128) return isalpha(*s) || *s=='_' ? s+1 : NULL;
   if (r=='d'+128) return isdigit(*s) ? s+1 : NULL;
@@ -36,7 +38,7 @@ char* pgen= NULL;
 char* parset_gen(char* r, char* s, char* p) {
   // TODO: use dstrprintf?
   char buf[16*1024]= {0};
-  printf("\n>>>>>\n");
+  if (debug>=1) printf("\n\t>>>>>\n");
   while(*r && *r++!=']') {
     if (*r=='$') {
       char n= *++r;
@@ -45,7 +47,7 @@ char* parset_gen(char* r, char* s, char* p) {
       else if (n=='$') strncat(buf, s, p-s);
     } else strncat(buf, r, 1);
   }
-  printf("%s\n<<<<<<\n", buf);
+  if (debug>=1) printf("\t%s\n<<<<<<\n", buf);
   // TODO: offset in res
   pgen= strdup(buf);
   return p;
@@ -64,7 +66,7 @@ char* parset(char target, char* s) {
   free(res[n]);
   // TODO: keep pointer+len???
   res[n]= pgen?pgen:strndup(s, r-s);
-  printf("  --(%d)= '%s'\n", n, res[n]);
+  if (debug) printf("\t'%c' ==> (%d)= '%s'\n", target, n, res[n]);
   nres= n+1;
   return r;
 }
@@ -76,7 +78,7 @@ char* parset_hlp(char target, char* s) {
   char* r= NT[target];
   int ok= 0;
   char* p= s;
-  printf("  '%s'\t'%s", p, r);
+  if (debug>1) printf("\t'%s'\t'%s", p, r);
   while(!eor(r)) {
     // space is always removed?
     // TODO: end?
@@ -95,7 +97,7 @@ char* parset_hlp(char target, char* s) {
       max= (c=='+'||c=='*'||delim)?9999:1;
       if (strchr("?*+,; ", c)) c= *++r;
       switch(c) {
-      case 'a': case 'd': case 'w': c=c+128; printf("\nCCCC %d\n", c); break;
+      case 'a': case 'd': case 'w': c=c+128; break;
       case '/': c=*++r; break; // TODO: rest is optional, how about gen?
       }
     }
@@ -112,9 +114,9 @@ char* parset_hlp(char target, char* s) {
       // failed: look for alt
       while(*r && *r++!='|');
       ok= 0, p= s;
-      printf("\nFAILED\n");
+      if (debug>1) printf("\n\tFAILED\n");
     } else ok= 1, r++; //, p= m, 
-    printf("  '%s'\t%s", p, r);
+    if (debug>=2) printf("\t'%s'\t%s", p, r);
   }
   return (ok && eor(r)) ? p : 0;
 }
@@ -132,52 +134,30 @@ char* parse(char target, char* s) {
 void test(char target, char* s) {
   char* p= parse(target, s);
 
-  printf("=== %s -> '%s'\n", s, p);
+  printf("\tTEST '%s' -> '%s'\n", s, p);
   for(int i=0; i<nres; i++)
-    printf("  [%d]= '%s'\n", i, res[i]);
-  printf("======================\n");
+    printf("\t[%d]= '%s'\n", i, res[i]);
+  printf("\t======================\n");
   // print previous
   for(int i=nres; i<256; i++)
-    if (res[i]) printf("  [%d]= '%s'\n", i, res[i]);
+    if (res[i]) printf("\t  [%d]= '%s'\n", i, res[i]);
   printf("\n");
 }
 
+void readparser(FILE* f) {
+  char rule, *ln= NULL; size_t z= 0;
+  while(getline(&ln, &z, f)>0){
+    if (ln && ln[strlen(ln)-1]=='\n') ln[strlen(ln)-1]= 0;
+    if (debug) printf("%s\n", ln);
+    if (!ln) break; if (!*ln) continue;
+    if (ln[1]=='=' && isalpha(*ln) && isupper(*ln)) NT[ln[0]]= strdup(ln+2);
+    else if (*ln=='#') ; // comment
+    else if (*ln=='?') rule=ln[1];
+    else test(rule, ln);
+  }
+  free(ln);
+}
+
 int main(void) {
-  // longer match first 33
-  //NT['D']= "0|1|2|33|3|4|5|6|7|8|9\n";
-  NT['D']= "0|1|2|3|4|5|6|7|8|9\n";
-  test('D', "0");
-  test('D', "1");
-  test('D', "7");
-  test('D', "3");
-  test('D', "33");
-  test('D', "44");
-  test('D', "a");
-
-  NT['N']= "DN|D\n";
-  test('N', "42");
-  test('N', "428");
-  test('N', "4");
-  test('N', "a428");
-
-  NT['Q']= "select N, N from int(N, N) i\n";
-  NT['Q']= "select N, N from int(N, N) i [ QUERY($$)\n COLS($1,$2)\n IOTA($3,$4) ]\n";
-  test('Q', "select 2222,123456 from int(1, 10) i");
-
-  NT['O']= "a$+ba";
-  // not work to match single letter!
-  test('O', "aa");
-  test('O', "aba");
-  test('O', "abba");
-
-  NT['I']= "$+a";
-  test('I', "foo");
-
-  NT['L']= "$,N";
-  test('L', "1,22,333,4444");
-
-  NT['W']= "$+a\n"; // TODO: err matches space lol... as it's skipped
-  NT['S']= "$*W";
-  
-  test('W', "foo bar fie fum");
+  readparser(stdin);
 }
