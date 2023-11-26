5dba4ab1f254b91ca27cabf64853f92d05ce1d9b
Jonas S Karlsson <jsk@yesco.org>
2023-11-26 23:48:45 +0700

    minizing, down to 85 lines even if added simple lex/dictionary!

--- parse.c
+++ parse.c
@@ -3,58 +3,77 @@
 #include <ctype.h>
 #include <assert.h>
 
-int debug= 1;
+int debug= 1; // DEBUG
+#define DEBUG(D) if (debug) do{D;}while(0);
+
+// simple dictionary
+#define DICTSIZE 16*1024
+char dict[DICTSIZE]= {0};
+// set/restore to do dynamic scoping!
+int dictix= 0;
+
+// register atom, set val first time
+long atom(char* s, long val) { long i= 0;
+  while(i<dictix) {
+    if (0==strcmp(s, dict+i)) return i;
+    i+= strlen(dict+i)+1+sizeof(val);
+  }
+  strcpy(dict+dictix, s); dictix+= strlen(s)+1;
+  memcpy(dict+dictix, &val, sizeof(val)); dictix+= sizeof(val);
+  return i;
+}
+
+// get value for atom
+long atomval(long i) {
+  long r;
+  memcpy(&r, dict+i+strlen(dict+i)+1, sizeof(r));
+  return r;
+}
 
 // char==name of NonTerminals
 char* NT[128]= {0};
 
-int eor(char* r) {
-  return !r || !*r || *r=='\n' || *r=='|';
-}
+int eor(char* r) { return !r || !*r || *r=='\n' || *r=='|'; }
  
-char* parset_hlp(char target, char* s);
-char* parset(char target, char* s);
+char* parset_hlp(char target, char* s); // DEBUG
+char* parset(char target, char* s); // DEBUG
 
 // match a char or a NT with S
+// $a=alpha $d=digit $w=word $e=end
 char* match(char r, char* s) {
-  // one space matches zero or more
-  //if (r==' ') { while(isspace(*s)) s++; return s; }
-  // spaceis always removed
-  if (debug>=3) printf("\n\tMATCH '%c' %d '%s'\n", r, r, s);
+  DEBUG(if (debug>=3) printf("\n\tMATCH '%c' %d '%s'\n", r, r, s))
+  // spaceis always removed (TODO remove?)
   while(isspace(*s)) s++;
   if (r=='a'+128) return isalpha(*s) || *s=='_' ? s+1 : NULL;
   if (r=='d'+128) return isdigit(*s) ? s+1 : NULL;
   if (r=='w'+128) return isalnum(*s) || *s=='_' ? s+1 : NULL;
   if (r=='e'+128) return !*s ? s : NULL;;
   if (r==*s) return s+1;
-  if (isupper(r)) return parset(r, s);
-  return NULL;
+  return r==*s?s+1 : isupper(r)?parset(r, s) : NULL;
 }
   
 #define MAXRES 256
 int nres= 0;
-char* res[MAXRES]= {0};
-char* pgen= NULL;
+char *res[MAXRES]= {0}, *pgen= NULL;
 
 char* parset_gen(char* r, char* s, char* p) {
   // TODO: use dstrprintf?
   char buf[16*1024]= {0}; // TODO: fix unsafe
-  //  if (debug>=1)
-  //printf("\n\tGEN>>>>>\n");
+  DEBUG(if (debug>=1) printf("\n\tGEN>>>>>\n"))
   while(*r && *r!=']') {
     if (*r=='$') {
       char n= *++r; r++;
       // TODO: offset in res
       if (isdigit(n)) {
-	printf("GEN: take $%c '%s'\n", n, res[n-'0']);
-	printf("  :  buf=%s\n", buf);
+	printf("GEN: take $%c '%s'\n", n, res[n-'0']); // DEBUG
+	printf("  :  buf=%s\n", buf); /// DEBUG
 	if (res[n-'0']) strcat(buf, res[n-'0']);
-	printf("  => buf=%s\n", buf);
+	printf("  => buf=%s\n", buf); // DEBUG
       }
       else if (n=='$') strncat(buf, s, p-s);
     } else strncat(buf, r++, 1);
   }
-  if (debug>=1) printf("\t%s\n<<<<<<\n", buf);
+  DEBUG(if (debug>=1) printf("\t%s\n<<<<<<\n", buf))
   // TODO: offset in res
   pgen= strdup(buf);
   return p;
@@ -63,43 +82,32 @@ char* parset_gen(char* r, char* s, char* p) {
 // parse a term
 // extract term match
 char* parset(char target, char* s) {
-  int n= nres++;
-  pgen= NULL;
-  char* r= parset_hlp(target, s);
-  nres--;
+  int n= nres++; pgen= NULL; char* r= parset_hlp(target, s); nres--;
   if (r==s) return NULL; // failed
   // sub rule matched - store
   assert(n<MAXRES);
-  free(res[n]);
   // TODO: keep pointer+len???
-  res[n]= pgen?pgen:strndup(s, r-s);
-  if (debug) printf("\t'%c' ==> (%d)= '%s'\n", target, n, res[n]);
-  nres= n+1;
-  return r;
+  free(res[n]); res[n]= pgen?pgen : strndup(s, r-s);
+  DEBUG(printf("\t'%c' ==> (%d)= '%s'\n", target, n, res[n]))
+  nres= n+1; return r;
 }
 
 // match TARGET rule to string S
 //   NULL if not match
 //   otherwise remaining string
 char* parset_hlp(char target, char* s) {
-  char* r= NT[target];
-  int ok= 0;
-  char* p= s;
-  if (debug>1) printf("\t'%s'\t'%s", p, r);
+  char *r= NT[target], *p= s; int ok= 0;
+  DEBUG(if (debug>1) printf("\t'%s'\t'%s", p, r))
   while(!eor(r)) {
-    // space is always removed?
-    // TODO: end?
-    while(isspace(*r)) r++;
-    while(isspace(*s)) s++;
+    // space is always removed? TODO: don't
+    while(isspace(*r)) r++; while(isspace(*s)) s++;
     // action == accept, gen out
     if (*r=='[') return parset_gen(r+1, s, p);
     //if (*r=='{') return eval(r, s, p); // TODO:
     // $, $; - delimited-list
-    int min=1, max=1, delim= 0;
-    char c= *r;
+    int min=1, max=1, delim= 0; char c= *r;
     if (*r=='$') {
-      c= *++r;
-      delim= strchr(",; ", c)?c:0;
+      c= *++r; delim= strchr(",; ", c)?c:0;
       min= (c=='?'||c=='*'||delim)?0:1;
       max= (c=='+'||c=='*'||delim)?9999:1;
       if (strchr("?*+,; ", c)) c= *++r;
@@ -110,7 +118,7 @@ char* parset_hlp(char target, char* s) {
     }
     if (*r=='\\') r++; // quoted char
 
-    char* m, *last= NULL;
+    char *m, *last= NULL;
     // TODO: concat captures?
     while(max-- && (m=match(c, p))) {
       if (*m && *m==delim) m++;
@@ -120,35 +128,28 @@ char* parset_hlp(char target, char* s) {
     if (min && !last) {
       // failed: look for alt
       while(*r && *r++!='|');
-      ok= 0, p= s;
-      if (debug>1) printf("\n\tFAILED\n");
+      ok= 0; p= s;
+      DEBUG(if (debug>1) printf("\n\tFAILED\n"))
     } else ok= 1, r++; //, p= m, 
-    if (debug>=2) printf("\t'%s'\t%s", p, r);
+    DEBUG(if (debug>=2) printf("\t'%s'\t%s", p, r))
   }
   return (ok && eor(r)) ? p : 0;
 }
 
 char* parse(char target, char* s) {
-  for(int i=0; i<MAXRES; i++)
-    if (res[i]) {
-      free(res[i]);
-      res[i]= NULL;
-    }
-  nres= 0;
+  for(int i=0; i<MAXRES; i++) { free(res[i]); res[i]= NULL; } nres= 0;
   return parset(target, s);
 }
   
 void test(char target, char* s) {
   char* p= parse(target, s);
 
-  printf("\t%s rule -> '%s'\n", p?"UNPARSED":"FAILED", p);
-  for(int i=0; i<nres; i++)
-    printf("\tRESULT[%d]= '%s'\n", i, res[i]);
-  printf("\t======================\n");
+  printf("\t%s rule -> '%s'\n", p?"UNPARSED":"FAILED", p); // DEBUG
+  for(int i=0; i<nres; i++) printf("\tRESULT[%d]= '%s'\n", i, res[i]); // DEBUG
+  printf("\t======================\n"); // DEBUG
   // print previous
-  for(int i=nres; i<256; i++)
-    if (res[i]) printf("\t  [%d]= '%s'\n", i, res[i]);
-  printf("\n");
+  for(int i=nres; i<256; i++) if (res[i]) printf("\t  [%d]= '%s'\n", i, res[i]); // DEBUG
+  printf("\n"); // DEBUG
 }
 
 // A=foo|bar|X
@@ -166,7 +167,7 @@ void readparser(FILE* f) {
   char rule, *ln= NULL; size_t z= 0, d='\n';
   while(getdelim(&ln, &z, d, f)>0){
     if (ln && ln[strlen(ln)-1]=='\n') ln[strlen(ln)-1]= 0; // rule
-    if (debug) printf("%s\n", ln);
+    DEBUG(printf("%s\n", ln))
     if (!ln) break; if (!*ln) continue;
     if (ln[1]=='=' && isalpha(*ln) && isupper(*ln)) NT[ln[0]]= strdup(ln+2);
     else if (*ln=='#') ; // comment
@@ -175,14 +176,27 @@ void readparser(FILE* f) {
     else if (*ln=='*') { rule=ln[1]; d=0; }
     else if (*ln=='$' && strchr("[{(<", ln[1])) {
       parset_gen(ln+1, "", NULL);
-      printf("GEN: %s\n", pgen);
+      printf("GEN: %s\n", pgen); // DEBUG
       free(pgen); pgen= NULL;
-    }
-    else test(rule, ln);
+    } else test(rule, ln);
   }
   free(ln);
 }
 
+// ENDWCOUNT
+
 int main(void) {
+  if (0) {
+    long nl= atom("NULL", 0);
+    long foo= atom("foo", 14);
+    long bar= atom("bar", 42);
+    printf("NULL=%ld foo=%ld bar=%ld\n", nl, foo, bar);
+    nl= atomval(nl);
+    foo= atomval(foo);
+    bar= atomval(bar);
+    printf("NULL=%ld foo=%ld bar=%ld\n", nl, foo, bar);
+    exit(3);
+  }
+
   readparser(stdin);
 }
