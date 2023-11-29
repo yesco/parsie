cd3070cf0a0aff25d348f01c5f50e6fbf6526ca5
Jonas S Karlsson <jsk@yesco.org>
2023-11-29 21:52:27 +0700

    pars ? seems to work...

--- pars.c
+++ pars.c
@@ -16,28 +16,38 @@ char* R[128]= {0};
 
 int eor(char* r) { return !r || !*r || *r=='\n' || *r=='|'; }
 
-#define Z ;goto again; case
+#define Z ;if(!s)return s; else goto next; case
 
-char* parse(char* r, char* s) { char*p= NULL;
- again: while(1) { DEBUG(printf("\tPARSE: '%s' (%d)\n\t    OF '%s'\n", r, *r, s))
+// n==-1 infinity
+// n==1 one time (for ? + *)
+// Returns:
+///  NULL=fail
+//   rest of string (unparsed) it's "" if done.
+char* parse(char* r, char* s, int n) { char*p= NULL;
+  DEBUG(printf("    PARSE '%s' '%s' %d\n", r, s, n));
+ next: while(n--) { DEBUG(printf("\tNEXT: '%s' (%d)\n\t   OF '%s' left=%d\n", r, *r, s, n))
   switch(*r) {
   case 0: case'\n': case'\r': case'|': return s;
   Z '(': Z '{': Z '[':
-  Z '?': Z '*': Z '+':
+    Z '?': p=s; s=parse(++r,s,1);
+    printf("\t??? s='%s'\n", s);
+    s=s?s:p
+  Z '*': Z '+':
   Z '%': switch(*++r){ // TODO: too long...
-    Z 'a': if (isalpha(*s)||*s=='_') s++; else return NULL;
-    Z 'd': if (isdigit(*s)) s++; else return NULL;
-    Z 'w': if (isalnum(*s)||*s=='_') s++; else return NULL;
+     Z 'a': if (isalpha(*s)||*s=='_') r++,s++; else return NULL;
+     Z 'd': if (isdigit(*s)) r++,s++; else return NULL;
+     Z 'w': if (isalnum(*s)||*s=='_') r++,s++; else return NULL;
   }
-  Z 'A'...'Z': if ((p=parse(R[*r++], s))) s= p; else return p;
+  Z 'A'...'Z': if ((p=parse(R[*r++], s, n))) s= p; else return p;
   Z '\\': r++;
   default: if (*s==*r++) s++; else while(*r && *r++!='|'); if(!*s) return s;
   }
   } // DEBUG
+  return s;
 }
 
 char* test(char rule, char* s) {
-  char *r= R[rule], *e= parse(r, s);
+  char *r= R[rule], *e= parse(r, s, -1);
   printf("    %%%s %c-> '%s'\n", e?(*e?"UNPARSED":"MATCHED!"):"FAILED", rule, e); // DEBUG
   return e;
 }
