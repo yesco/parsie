61b5e2db80d2338c68fe6884769f08d609d73b50
Jonas S Karlsson <jsk@yesco.org>
2023-12-28 21:53:25 +0700

    alf fib opt(itimize) better

--- alf.c
+++ alf.c
@@ -119,7 +119,9 @@ default: P("\n[%% Undefined op: '%s']\n", p-1);p++;exit(3);} goto next;
 // fib 19-27% faster!
 char* opt(char* p) { char *s= p; while((*s)) { if (*s=='"') while(*++s!='"'){};
   if (isspace(s[1])) {if(isdigit(s[0]) && isdigit(s[2])) ;// NOT
-  else memmove(s+1, s+2, strlen(s+2)+1); } s++; } return p; }
+  else memmove(s+1, s+2, strlen(s+2)+1); } else s++; }
+  printf("%s\n", p);
+  return p; }
 
 
 // ENDWCOUNT
